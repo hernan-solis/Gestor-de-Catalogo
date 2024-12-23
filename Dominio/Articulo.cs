1035962e8eefe841cf55e6881f65e4d159548b89
Hache94k <hernansolis94k@gmail.com>
2024-12-22 22:15:57 -0300

    version 0

---
+++ Dominio/Articulo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Dominio
+{
+    internal class Articulo
+    {
+        public int Id { get; set; }
+        public int Codigo { get; set; }
+        public String Nombre { get; set; }
+        public String Descripcion { get; set; }
+        public Marca Marca { get; set; }
+        public Categoria Categoria { get; set; }
+        public String ImagenUrl { get; set; }
+        public decimal Precio { get; set; }
+
+    }
+}
