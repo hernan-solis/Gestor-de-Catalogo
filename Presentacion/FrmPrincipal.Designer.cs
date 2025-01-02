891676ef709b75a13db31398324940e55f663423
Hache94k <hernansolis94k@gmail.com>
2025-01-01 23:14:20 -0300

    version 3 - filtros

--- Presentacion/FrmPrincipal.Designer.cs
+++ Presentacion/FrmPrincipal.Designer.cs
@@ -37,10 +37,18 @@
             this.lblImagen = new System.Windows.Forms.Label();
             this.gbxArticuloSeleccionado = new System.Windows.Forms.GroupBox();
             this.gbxNuevo = new System.Windows.Forms.GroupBox();
+            this.lblCampo = new System.Windows.Forms.Label();
+            this.lblCriterio = new System.Windows.Forms.Label();
+            this.lblFiltro = new System.Windows.Forms.Label();
+            this.cbxCampo = new System.Windows.Forms.ComboBox();
+            this.cbxCriterio = new System.Windows.Forms.ComboBox();
+            this.tbxFiltro = new System.Windows.Forms.TextBox();
+            this.gbxFiltro = new System.Windows.Forms.GroupBox();
             ((System.ComponentModel.ISupportInitialize)(this.dgvArticulos)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbxImagenPrincipal)).BeginInit();
             this.gbxArticuloSeleccionado.SuspendLayout();
             this.gbxNuevo.SuspendLayout();
+            this.gbxFiltro.SuspendLayout();
             this.SuspendLayout();
             // 
             // dgvArticulos
@@ -49,7 +57,7 @@
             this.dgvArticulos.BackgroundColor = System.Drawing.SystemColors.ControlLightLight;
             this.dgvArticulos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dgvArticulos.GridColor = System.Drawing.SystemColors.Control;
-            this.dgvArticulos.Location = new System.Drawing.Point(3, 40);
+            this.dgvArticulos.Location = new System.Drawing.Point(115, 37);
             this.dgvArticulos.MultiSelect = false;
             this.dgvArticulos.Name = "dgvArticulos";
             this.dgvArticulos.ReadOnly = true;
@@ -60,7 +68,7 @@
             // 
             // pbxImagenPrincipal
             // 
-            this.pbxImagenPrincipal.Location = new System.Drawing.Point(626, 40);
+            this.pbxImagenPrincipal.Location = new System.Drawing.Point(738, 37);
             this.pbxImagenPrincipal.Name = "pbxImagenPrincipal";
             this.pbxImagenPrincipal.Size = new System.Drawing.Size(334, 335);
             this.pbxImagenPrincipal.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -69,59 +77,66 @@
             // 
             // btnAgregar
             // 
-            this.btnAgregar.Location = new System.Drawing.Point(61, 41);
+            this.btnAgregar.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.btnAgregar.Location = new System.Drawing.Point(16, 28);
             this.btnAgregar.Name = "btnAgregar";
             this.btnAgregar.Size = new System.Drawing.Size(75, 23);
             this.btnAgregar.TabIndex = 2;
             this.btnAgregar.Text = "Agregar";
-            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.UseVisualStyleBackColor = false;
             this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
             // 
             // btnModificar
             // 
-            this.btnModificar.Location = new System.Drawing.Point(6, 41);
+            this.btnModificar.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.btnModificar.Location = new System.Drawing.Point(16, 42);
             this.btnModificar.Name = "btnModificar";
             this.btnModificar.Size = new System.Drawing.Size(75, 23);
             this.btnModificar.TabIndex = 3;
             this.btnModificar.Text = "Modificar";
-            this.btnModificar.UseVisualStyleBackColor = true;
+            this.btnModificar.UseVisualStyleBackColor = false;
             this.btnModificar.Click += new System.EventHandler(this.btnModificar_Click);
             // 
             // btnEliminar
             // 
-            this.btnEliminar.Location = new System.Drawing.Point(119, 41);
+            this.btnEliminar.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.btnEliminar.Location = new System.Drawing.Point(16, 86);
             this.btnEliminar.Name = "btnEliminar";
             this.btnEliminar.Size = new System.Drawing.Size(75, 23);
             this.btnEliminar.TabIndex = 4;
             this.btnEliminar.Text = "Eliminar";
-            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.UseVisualStyleBackColor = false;
             this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
             // 
             // lblArticulos
             // 
             this.lblArticulos.AutoSize = true;
-            this.lblArticulos.Location = new System.Drawing.Point(261, 14);
+            this.lblArticulos.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblArticulos.Location = new System.Drawing.Point(373, 11);
             this.lblArticulos.Name = "lblArticulos";
-            this.lblArticulos.Size = new System.Drawing.Size(47, 13);
+            this.lblArticulos.Size = new System.Drawing.Size(110, 20);
             this.lblArticulos.TabIndex = 5;
-            this.lblArticulos.Text = "Articulos";
+            this.lblArticulos.Text = "ARTICULOS";
             // 
             // lblImagen
             // 
             this.lblImagen.AutoSize = true;
-            this.lblImagen.Location = new System.Drawing.Point(754, 14);
+            this.lblImagen.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblImagen.Location = new System.Drawing.Point(828, 11);
             this.lblImagen.Name = "lblImagen";
-            this.lblImagen.Size = new System.Drawing.Size(89, 13);
+            this.lblImagen.Size = new System.Drawing.Size(176, 20);
             this.lblImagen.TabIndex = 6;
-            this.lblImagen.Text = "Imagen Asociada";
+            this.lblImagen.Text = "IMAGEN ASOCIADA";
             // 
             // gbxArticuloSeleccionado
             // 
+            this.gbxArticuloSeleccionado.BackColor = System.Drawing.SystemColors.ControlLightLight;
             this.gbxArticuloSeleccionado.Controls.Add(this.btnEliminar);
             this.gbxArticuloSeleccionado.Controls.Add(this.btnModificar);
-            this.gbxArticuloSeleccionado.Location = new System.Drawing.Point(329, 470);
+            this.gbxArticuloSeleccionado.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gbxArticuloSeleccionado.Location = new System.Drawing.Point(2, 167);
             this.gbxArticuloSeleccionado.Name = "gbxArticuloSeleccionado";
-            this.gbxArticuloSeleccionado.Size = new System.Drawing.Size(200, 100);
+            this.gbxArticuloSeleccionado.Size = new System.Drawing.Size(105, 130);
             this.gbxArticuloSeleccionado.TabIndex = 7;
             this.gbxArticuloSeleccionado.TabStop = false;
             this.gbxArticuloSeleccionado.Text = "Articulo Seleccionado";
@@ -129,19 +144,95 @@
             // 
             // gbxNuevo
             // 
+            this.gbxNuevo.BackColor = System.Drawing.SystemColors.ControlLightLight;
             this.gbxNuevo.Controls.Add(this.btnAgregar);
-            this.gbxNuevo.Location = new System.Drawing.Point(33, 470);
+            this.gbxNuevo.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gbxNuevo.Location = new System.Drawing.Point(2, 72);
             this.gbxNuevo.Name = "gbxNuevo";
-            this.gbxNuevo.Size = new System.Drawing.Size(200, 100);
+            this.gbxNuevo.Size = new System.Drawing.Size(105, 66);
             this.gbxNuevo.TabIndex = 5;
             this.gbxNuevo.TabStop = false;
             this.gbxNuevo.Text = "Articulo Nuevo";
+            // 
+            // lblCampo
+            // 
+            this.lblCampo.AutoSize = true;
+            this.lblCampo.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCampo.Location = new System.Drawing.Point(13, 24);
+            this.lblCampo.Name = "lblCampo";
+            this.lblCampo.Size = new System.Drawing.Size(49, 13);
+            this.lblCampo.TabIndex = 8;
+            this.lblCampo.Text = "Campo:";
+            // 
+            // lblCriterio
+            // 
+            this.lblCriterio.AutoSize = true;
+            this.lblCriterio.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCriterio.Location = new System.Drawing.Point(228, 24);
+            this.lblCriterio.Name = "lblCriterio";
+            this.lblCriterio.Size = new System.Drawing.Size(51, 13);
+            this.lblCriterio.TabIndex = 9;
+            this.lblCriterio.Text = "Criterio:";
+            // 
+            // lblFiltro
+            // 
+            this.lblFiltro.AutoSize = true;
+            this.lblFiltro.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFiltro.Location = new System.Drawing.Point(448, 24);
+            this.lblFiltro.Name = "lblFiltro";
+            this.lblFiltro.Size = new System.Drawing.Size(39, 13);
+            this.lblFiltro.TabIndex = 10;
+            this.lblFiltro.Text = "Filtro:";
+            // 
+            // cbxCampo
+            // 
+            this.cbxCampo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbxCampo.FormattingEnabled = true;
+            this.cbxCampo.Location = new System.Drawing.Point(68, 16);
+            this.cbxCampo.Name = "cbxCampo";
+            this.cbxCampo.Size = new System.Drawing.Size(121, 21);
+            this.cbxCampo.TabIndex = 11;
+            this.cbxCampo.SelectedIndexChanged += new System.EventHandler(this.cbxCampo_SelectedIndexChanged);
+            // 
+            // cbxCriterio
+            // 
+            this.cbxCriterio.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbxCriterio.FormattingEnabled = true;
+            this.cbxCriterio.Location = new System.Drawing.Point(285, 16);
+            this.cbxCriterio.Name = "cbxCriterio";
+            this.cbxCriterio.Size = new System.Drawing.Size(121, 21);
+            this.cbxCriterio.TabIndex = 12;
+            // 
+            // tbxFiltro
+            // 
+            this.tbxFiltro.Location = new System.Drawing.Point(493, 17);
+            this.tbxFiltro.Name = "tbxFiltro";
+            this.tbxFiltro.Size = new System.Drawing.Size(143, 20);
+            this.tbxFiltro.TabIndex = 13;
+            // 
+            // gbxFiltro
+            // 
+            this.gbxFiltro.Controls.Add(this.cbxCampo);
+            this.gbxFiltro.Controls.Add(this.tbxFiltro);
+            this.gbxFiltro.Controls.Add(this.lblCampo);
+            this.gbxFiltro.Controls.Add(this.cbxCriterio);
+            this.gbxFiltro.Controls.Add(this.lblCriterio);
+            this.gbxFiltro.Controls.Add(this.lblFiltro);
+            this.gbxFiltro.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gbxFiltro.Location = new System.Drawing.Point(96, 382);
+            this.gbxFiltro.Name = "gbxFiltro";
+            this.gbxFiltro.Size = new System.Drawing.Size(661, 43);
+            this.gbxFiltro.TabIndex = 3;
+            this.gbxFiltro.TabStop = false;
+            this.gbxFiltro.Text = "Filtro";
+            this.gbxFiltro.Enter += new System.EventHandler(this.gbxFiltro_Enter);
             // 
             // FrmPrincipal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(973, 693);
+            this.ClientSize = new System.Drawing.Size(1079, 437);
+            this.Controls.Add(this.gbxFiltro);
             this.Controls.Add(this.gbxNuevo);
             this.Controls.Add(this.gbxArticuloSeleccionado);
             this.Controls.Add(this.lblImagen);
@@ -159,6 +250,8 @@
             ((System.ComponentModel.ISupportInitialize)(this.pbxImagenPrincipal)).EndInit();
             this.gbxArticuloSeleccionado.ResumeLayout(false);
             this.gbxNuevo.ResumeLayout(false);
+            this.gbxFiltro.ResumeLayout(false);
+            this.gbxFiltro.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -175,5 +268,12 @@
         private System.Windows.Forms.Label lblImagen;
         private System.Windows.Forms.GroupBox gbxArticuloSeleccionado;
         private System.Windows.Forms.GroupBox gbxNuevo;
+        private System.Windows.Forms.Label lblCampo;
+        private System.Windows.Forms.Label lblCriterio;
+        private System.Windows.Forms.Label lblFiltro;
+        private System.Windows.Forms.ComboBox cbxCampo;
+        private System.Windows.Forms.ComboBox cbxCriterio;
+        private System.Windows.Forms.TextBox tbxFiltro;
+        private System.Windows.Forms.GroupBox gbxFiltro;
     }
 }
--- Presentacion/FrmPrincipal.cs
+++ Presentacion/FrmPrincipal.cs
@@ -9,6 +9,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+    
 
 namespace Presentacion
 {
@@ -24,7 +25,10 @@ namespace Presentacion
         private void FrmPrincipal_Load(object sender, EventArgs e)
         {
             cargarBase();
-
+            cargarListaCampo();
+            cargarListaCriterio(cbxCampo.Text);
+            
+            
             
   
         }
@@ -89,7 +93,15 @@ namespace Presentacion
 
             ArticuloNegocio articuloNegocio = new ArticuloNegocio();
 
-            articuloNegocio.eliminarArticulo(articuloSeleccionado);
+            DialogResult resultado = MessageBox.Show("Seguro desea eliminar el articulo seleccionado?", "Advertencia", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+
+            if (resultado == DialogResult.Yes)
+            {
+                articuloNegocio.eliminarArticulo(articuloSeleccionado);
+            }
+            
+
+            
 
 
             cargarBase();
@@ -101,6 +113,49 @@ namespace Presentacion
             frmAlta.ShowDialog();
             cargarBase();
 
+        }
+
+        private void gbxFiltro_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cargarListaCampo() 
+        {
+            List<String> campos = new List<String>();
+            campos.Add("Codigo");
+            campos.Add("Nombre");
+            campos.Add("Descripcion");
+            campos.Add("Marca");
+            campos.Add("Categoria");
+            campos.Add("Precio");
+
+            cbxCampo.DataSource = campos;
+
+        }
+        private void cargarListaCriterio(String campo) 
+        {
+            List<String> listaCriterios = new List<String>();
+
+            if (campo == "Precio")
+            {
+                listaCriterios.Add("Mayor a");
+                listaCriterios.Add("Menor a");
+                listaCriterios.Add("Igual a");
+            }
+            else 
+            {
+                listaCriterios.Add("Contiene");
+                listaCriterios.Add("Empieza con");
+                listaCriterios.Add("Termina con");
+            }
+
+            cbxCriterio.DataSource = listaCriterios;
+        }
+
+        private void cbxCampo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            cargarListaCriterio(cbxCampo.Text);
         }
     }
 }
